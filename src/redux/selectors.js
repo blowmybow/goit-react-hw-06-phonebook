72b8acf8fa196ae1dfebaef309e66606c0609aab
Volodymyr Horpynchenko <115591313+blowmybow@users.noreply.github.com>
2023-06-27 13:38:33 +0300

    update selectors

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,33 +1,17 @@
-import { toast } from 'react-toastify';
-import notifyOptions from '../notify/NotifyOptions';
+import { createSelector } from '@reduxjs/toolkit';
 
 export const getContacts = store => store.contacts;
 
 export const getFilter = store => store.filter;
 
-export const getFilteredContacts = state => {
-  const contacts = getContacts(state);
-  const filter = getFilter(state);
-
-  // const normalizedFilter = filter.toLowerCase();
-  //   return contacts.filter(contact =>
-  //     contact.name.toLowerCase().includes(normalizedFilter)
-  //   );
-  // if (contacts !== undefined) {
-  //   return;
-  // }
-  if (!filter) {
-    return contacts;
+export const getFilteredContacts = createSelector(
+  [getContacts, getFilter],
+  (contacts, filter) => {
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(
+      ({ name, number }) =>
+        name.toLowerCase().trim().includes(normalizedFilter) ||
+        number.trim().includes(normalizedFilter)
+    );
   }
-  const normalizedFilter = filter.toLowerCase();
-  const filteredContacts = contacts.filter(
-    ({ name, number }) =>
-      name.toLowerCase().trim().includes(normalizedFilter) ||
-      number.trim().includes(normalizedFilter)
-  );
-
-  if (normalizedFilter && !filteredContacts.length) {
-    toast.warn(`No contacts matching your request`, notifyOptions);
-  }
-  return filteredContacts;
-};
+);
